59e3109b1724c2d58d74a409111cb367657ebee7
hnam1108 <87578553+hnam1108@users.noreply.github.com>
2021-07-18 00:33:02 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,16 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    introTitle: 'Phương à!',
+    introDesc: `nãy m bảo t bede chán xong thích mất tích kh`,
     btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
-    btnAccept: 'Okiiiii lun <3',
+    title: 'T có be de kh hay đàn ông',
+    desc: 'good man ',
+    btnYes: 'đàn ông đẹp trai 10 điểm',
+    btnNo: 'be de vcl',
+    question:'t biết mà muốn nói thêm gì kh',
+    btnReply: 'hihi',
+    reply: 'nam đẹp trai 10 điểm',
+    mess: 't biết mà hihi',
+    messDesc: 'thế thì bao h mua kem cho.',
+    btnAccept: 'ok hihihihihhi',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
